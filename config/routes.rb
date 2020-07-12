eaa1a9bc521c5752f3416465cf4d69ea8524d8ed
Alicia <sample@email.com>
2020-07-12 09:56:04 +0000

    routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get  '/signup', to: 'users#new'
   
-  # ログイン機能
-  get    '/login', to: 'sessions#new'
-  post   '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  
 end
