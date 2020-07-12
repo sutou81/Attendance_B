ab6a6a8ad461d338b19eb5173beae5e427be5f3a
Alicia <sample@email.com>
2020-07-12 09:35:06 +0000

    signup初期設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessons/new'
   root 'static_pages#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  '/signup', to: 'users#new'
+  
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
